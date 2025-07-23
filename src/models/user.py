eec2eb42fb354eb201cfe689c7ff6120bbd82dde
Juan Camilo Tovar <tovarcortesjuancamilo29@gmail.com>
2025-07-23 15:10:14 -0500

    actualizacion 23 de julio

---
+++ src/models/user.py
@@ -0,0 +1,7 @@
+class User():
+    
+    def __init__(self,id,correo,contrasena,nombre):
+        self.id = id
+        self.correo = correo
+        self.contrasena = contrasena
+        self.nombre = nombre
